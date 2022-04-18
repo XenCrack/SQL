6eaff6aa8719463ea13a91792f788ef9b0034411
Oğuz Muhlis Sayın <91703815+XenCrack@users.noreply.github.com>
2022-04-18 17:12:27 +0300

    Update temp-table.sql

--- SQL/temp-table.sql
+++ SQL/temp-table.sql
@@ -1,10 +1,10 @@
-create table #mert(
+create table #CRACK(
 	id int,
 	name nvarchar(50),
 	surname nvarchar(50),
 	emailAddress nvarchar(50)
 )
-insert into #mert
+insert into #CRACK
 select 
 top 5
 p.BusinessEntityID,
@@ -14,13 +14,13 @@ e.EmailAddress
 from Person.Person p
 inner join Person.EmailAddress e
 on p.BusinessEntityID = e.BusinessEntityID
---insert into #mert(id,name) select id,name from Department
---insert into #mert(id,name) values(1,'adasd')
---insert into #mert(id,name) values(2,'adasd')
---insert into #mert(id,name) values(3,'adasd')
---insert into #mert(id,name) values(4,'adasd')
---insert into #mert(id,name) values(5,'adasd')
+--insert into #CRACK(id,name) select id,name from Department
+--insert into #CRACK(id,name) values(1,'adasd')
+--insert into #CRACK(id,name) values(2,'adasd')
+--insert into #CRACK(id,name) values(3,'adasd')
+--insert into #CRACK(id,name) values(4,'adasd')
+--insert into #CRACK(id,name) values(5,'adasd')
 
-update #mert set name = 'Önder' where id = 2
-select * from #mert
-drop table #mert
+update #CRACK set name = 'Ã–nder' where id = 2
+select * from #CRACK
+drop table #CRACK
